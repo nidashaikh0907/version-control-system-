53e54f440c665fd4e501457cfcfc990f7d71b6a5
Nida Shaikh <nidashaikh1923@gmail.com>
2026-08-04 21:52:47 +0530

    created revert commit functionality

--- backend/controllers/revert.js
+++ backend/controllers/revert.js
@@ -1,5 +1,27 @@
-async function revertRepo() {
-    console.log("reverted successfully");
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util"); //allow us to check for the existing things
+
+const readdir = promisify(fs.readdir); //craete modified version of readdir
+const copyFile = promisify(fs.copyFile); //craete modified version of copyFile
+
+async function revertRepo(commitID) {
+  const repoPath = path.resolve(process.cwd(), ".hiddenGit");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    const commitDir = path.join(commitsPath, commitID); //creats a path of commitID inside commits
+    const files = await readdir(commitDir); //read  all files from commitID folder
+    const parentDir = path.resolve(repoPath, ".."); //get the parent directory of .hiddenGit
+
+    for (const file of files) {
+      //read each file
+      await copyFile(path.join(commitDir, file), path.join(parentDir, file)); //transfer the file from commitID folder to parent directory of .hiddenGit
+    }
+    console.log(`Reverted to commit ${commitID}`);
+  } catch (err) {
+    console.error("Error reverting commit:", err);
+  }
 }
 
-module.exports={revertRepo};
+module.exports = { revertRepo };
--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,9 @@ yargs(hideBin(process.argv))
         type: "string",
       });
     },
-    revertRepo,
+   (argv)=>{
+      revertRepo(argv.commitID);
+    }
   )
   .demandCommand(1, "You need atleast one command")
   .help().argv;
