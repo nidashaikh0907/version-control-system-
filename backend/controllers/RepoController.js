f7a94910237b6e7e77aff454c39cbdffd9a62e22
Nida Shaikh <nidashaikh1923@gmail.com>
2026-08-12 21:40:11 +0530

    Implemented repository controller logic

--- backend/controllers/RepoController.js
+++ backend/controllers/RepoController.js
@@ -76,24 +76,88 @@ async function fetchedRepositoryByName(req, res) {
     }
     res.json(repositoryByName);
   } catch (err) {
-    console.error("error during fetching repositories!:", err.message);
+    console.error("error during fetching repositories!", err.message);
     res.status(500).send("server error!");
   }
 }
 
-const fetchedRepositoryForCurrentUser = (req, res) => {};
+async function fetchedRepositoryForCurrentUser(req, res) {
+  const userId = req.user;
+  console.log(req.user);
+  try {
+    const repositories = await Repository.find({ owner: userId });
+    if (!repositories || repositories.length == 0) {
+      return res.status(404).json({ error: "User Repositories not found" });
+    }
+    res.json({ message: "Repository found!", repositories });
+  } catch (err) {
+    console.error("error during fetching user repositories:", err.message);
+    res.status(500).send("server error!");
+  }
+}
 
-const updateRepository = (req, res) => {
-  res.send("Repository Created!");
-};
+async function updateRepositoryById(req, res) {
+  const { id } = req.params;
+  const { content, description } = req.body;
 
-const toogleVisibilityById = (req, res) => {
-  res.send("Repository Created!");
-};
+  try {
+    const repository = await Repository.findById(id);
 
-const deleteRepository = (req, res) => {
-  res.send("Repository Created!");
-};
+    if (!repository) {
+      return res.status(404).json({ error: "Repository not found" });
+    }
+
+    repository.content.push(content);
+    repository.description = description;
+
+    const updatedRepository = await repository.save();
+
+    res.json({
+      message: "Repository updated successfully",
+      repository: updatedRepository,
+    });
+  } catch (err) {
+    console.error("error during updating repository:", err.message);
+    res.status(500).send("server error!");
+  }
+}
+
+async function toogleVisibilityById(req, res) {
+  const { id } = req.params;
+
+  try {
+    const repository = await Repository.findById(id);
+    if (!repository) {
+      return res.status(404).json({ error: "Repository not found" });
+    }
+
+    repository.visibility = !repository.visibility;
+
+    const updatedRepository = await Repository.save();
+
+    res.json({
+      message: "Repository visibility toggled Successfullty",
+      repository: updatedRepository,
+    });
+  } catch (err) {
+    console.error("error during toggling visibility repository:", err.message);
+    res.status(500).send("server error!");
+  }
+}
+
+async function deleteRepository(req, res) {
+  const { id } = req.params;
+  try {
+    const Repository = await Repository.findByIdAndDelete(id);
+    if (!Repository) {
+      return res.status(400).json({ message: "Repository not found!" });
+    }
+    res.json({ message: "Repository Deleted Successfully" });
+  } catch (err) {
+    console.error("error during deleting repository:", err.message);
+    res.status(500).send("server error!");
+  }
+}
 
 module.exports = {
   createRepository,
