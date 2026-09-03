6100720157da392edafd02fbfc0f1c55ea63c88e
Nida Shaikh <nidashaikh1923@gmail.com>
2026-09-03 20:47:32 +0530

    Allow Render host in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    preview: {
+    allowedHosts: ['version-control-system-1-a2ix.onrender.com'],
+  },
+  server: {
+    allowedHosts: ['version-control-system-1-a2ix.onrender.com'],
+  },
 })
